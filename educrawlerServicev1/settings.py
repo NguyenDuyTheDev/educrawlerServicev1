fbbc3cdee11c2741cd34a4c69fe8c24988782146
NguyenDuyTheDev <125572099+NguyenDuyTheDev@users.noreply.github.com>
2024-03-08 07:54:43 +0700

    fix bug

--- educrawlerServicev1/settings.py
+++ educrawlerServicev1/settings.py
@@ -52,9 +52,9 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "educrawlerServicev1.middlewares.Educrawlerservicev1DownloaderMiddleware": 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    "educrawlerServicev1.middlewares.Educrawlerservicev1DownloaderMiddleware": 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
